024d231a7733e5222e4c3f0f3d19c7f586fa555d
Abhi789X <87174484+Abhi789X@users.noreply.github.com>
2025-07-17 15:55:25 +0530

    Create tailwaind.config.js

---
+++ tailwaind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        darkred: "#1e0f0f",
+        blood: "#7e1e1e",
+      }
+    },
+  },
+  plugins: [],
+};
